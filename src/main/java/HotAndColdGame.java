7503965f6a30d6ab84fa343f2b3fa3b3838aa8cd
Jack Chen <23hchen@athenian.org>
2022-02-27 20:43:25 -0800

    Finished

--- src/main/java/HotAndColdGame.java
+++ src/main/java/HotAndColdGame.java
@@ -8,6 +8,11 @@ public class HotAndColdGame {
     JFrame frame;
     JPanel panel1;
     JTextField field1;
+    int x = (int) (Math.random()*500);
+    int y = (int) (Math.random()*500);
+    double h = Integer.MAX_VALUE;
+    String i = "";
+    String p = "";
 
     public HotAndColdGame() {
         // Set up the frame
@@ -26,7 +31,39 @@ public class HotAndColdGame {
         panel1.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                double k = 0;
 
+                k = Math.sqrt(Math.pow(e.getX() - x, 2) + Math.pow(e.getY() - y, 2));
+
+
+                if (k > 50){
+                    i = ("Very Cold");
+                }
+                else if ((k > 30)&&(k < 50)){
+                    i = ("Cold");
+                }
+                else if ((k > 15)&&(k < 30)){
+                    i = ("Room Temperature");
+                }
+                else if ((k > 8)&&(k < 15)){
+                    i = ("Hot");
+                }
+                else if ((k > 3)&&(k < 8)){
+                    i = ("Very Hot");
+                }
+                else if (k == 0){
+                    i = ("Perfect!");
+                }
+
+                if (k < h){
+                    p = "Hotter Than Last Time!";
+                }
+                if (k > h){
+                    p = "Colder Than Last Time!";
+                }
+
+                field1.setText( i + " [" + p + "]");
+                h = k;
             }
 
             @Override
--- src/main/java/KeyCounter.java
+++ src/main/java/KeyCounter.java
@@ -6,9 +6,46 @@ import static java.awt.event.KeyEvent.*;
 
 public class KeyCounter {
     JFrame frame;
+    JButton button1;
+    JTextField field1, field2;
+    int w = 0;
 
     public KeyCounter() {
-        // YOUR CODE HERE
+        // Set up the frame
+        frame = new JFrame("Key Counter");
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Need to create this before button ActionListener code
+        field1 = new JTextField();
+        field2 = new JTextField();
+
+        button1 = new JButton("Calculate Total Presses:");
+
+        frame.add(field1);
+        frame.add(button1);
+        frame.add(field2);
+
+        button1.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            w = w +1;
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                field2.setText(("" + w));
+
+            }
+        });
+
+        // pack and show
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
